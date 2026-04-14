d59a84262d35290e471919734bc9eeb4e90c0e61
Kami Vilela <vilelakamy18l@gmail.com>
2026-04-14 15:32:20 -0300

    feat: criando layout da tela de login com inputs e links

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import styles from "./Login.module.css";
 
 function Login() {
     // useState pra armazenar e escrever os campos de usuario e login
@@ -13,22 +14,30 @@ function Login() {
 
     return (
         // tela do login
-        <div>
-            <h2 className="task-title">LOGIN</h2>
-            {/* campo de usuário */}
-            <input
-                type='text'
-                placeholder='Nome de usuário *'
-                onChange={(e) => setUsuario(e.target.value)}
-            />
-            {/* campo de senha */}
-            <input 
-                type='password'
-                placeholder='Senha *'
-                onChange={(e) => setSenha(e.target.value)} 
-            />
-            {/* botão de entrar */}
-            <button onClick={handleLogin}>Entrar</button>
+        <div className={styles.container}>
+            <div className={styles.left}></div>
+            <div className={styles.right}>
+                <h2 className="task-title">LOGIN</h2>
+                {/* campo de usuário */}
+                <input
+                    type='text'
+                    placeholder='Nome de usuário *'
+                    onChange={(e) => setUsuario(e.target.value)}
+                />
+                {/* campo de senha */}
+                <input 
+                    type='password'
+                    placeholder='Senha *'
+                    onChange={(e) => setSenha(e.target.value)} 
+                />
+
+                <a href="#">Esqueceu a senha?</a>
+
+                {/* botão de entrar */}
+                <button onClick={handleLogin}>Entrar</button>
+
+                <a href="#">Não tem uma conta? <span>Criar Conta</span></a>
+            </div>
         </div>
     );
 }
