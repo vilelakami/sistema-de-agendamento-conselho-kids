4f11e2618f048e97e84b85f62aa981ad0bef2a50
Kami Vilela <vilelakamy18l@gmail.com>
2026-04-19 18:21:56 -0300

    feat: adicionando lógica, estilização e finalidades no dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import styles from "./css/Dashboard.module.css";
 import React from "react";
+// importando botão reutilizável da pasta dos components
 import Button from "../components/Button/Button";
-
+import CalendarIcon from "../assets/icons/calendar.svg";
 
 function Dashboard() {
+    // estados
     const [linhaExpandida, setLinhaExpandida] = useState(null);
+    const [filtroStatus, setFiltroStatus] = useState("todos");
+    const [menuStatusAberto, setMenuStatusAberto] = useState(false);
+    const [menuDataAberto, setMenuDataAberto] = useState(false);
 
-    //simulando dados
-    //criando um obj
+    // editando dt e hora do agendamento
+    const [editandoId, setEditandoId] = useState(null);
+    const [dataInput, setDataInput] = useState("");
+    const [horaInput, setHoraInput] = useState("");
+
+    // dados (exemplo)
     const [agendamentos, setAgendamentos] = useState([
         {
             id: 8,
@@ -33,14 +42,76 @@ function Dashboard() {
             status: "concluido",
             dataAgendamento: "31/03/2025 11:30h",
             filhos: ["Lucas - 05/05/2020"]
+        },
+        {
+            id: 10,
+            responsavel: "Carlos Oliveira",
+            cpf: "111.111.111-11",
+            cep: "05555-000",
+            comoConheceu: "Indicação",
+            dataCriacao: "15/04/2026",
+            quantidadeFilhos: 1,
+            status: "pendente", 
+            dataAgendamento: null, 
+            filhos: ["Enzo - 12/02/2018"]
         }
     ]);
 
-    // função que expande a linha para mostrar "filhos"
+    // função que salva a edição da data e hora
+    const salvarEdicao = (id, novoStatus) => {
+        //se nao preencher nada volta o ícone
+        if (!dataInput.trim() || !horaInput.trim()) {
+        setEditandoId(null);
+        return;
+    }
+        //arrow function que percorre todos os ids dos responsáveis
+        const novaLista = agendamentos.map(item => {
+            //o id que eu quero editar a data é o mesmo do responsável?
+            if (item.id === id) {
+                //transf em padrão br, separa em array [2004,03,18], inverter [18,03,2004] e aplica as barras /
+                const dataFormatada = dataInput.split('-').reverse().join('/');
+                //
+                return { 
+                    //spread operator: pega todas as infos desse item
+                    ...item, 
+                    //modifica o campo e agendamento e os status
+                    dataAgendamento: `${dataFormatada} ${horaInput}`,
+                    status: novoStatus
+                };
+            }
+            //se o if for falso retorna apenas o ícone de calendário
+            return item;
+        });
+        //atribuo a nova lista pro Agendamento
+        setAgendamentos(novaLista);
+        //limpo os campos
+        setEditandoId(null);
+        setDataInput("");
+        setHoraInput("");
+    };
+
+    //lógica dos filtros
+    //criando uma lista temporária para verificar os status
+    const agendamentosFiltrados = agendamentos.filter(item => {
+        //verificando todos da lista, se o filtro for "todos" retorna todos
+        if (filtroStatus === "todos") return true;
+        // caso contrário ele verifica se a lista tem o status que eu cliquei e verifica
+        return item.status === filtroStatus;
+    });
+
+    //função pra expandir a linha assim que apertar em "alunos>"
     const toggleLinha = (id) => {
+        //o id do responsavel é o que eu to cliando? se sim, expande, se não mostra apenas a linha do responsável
         setLinhaExpandida(linhaExpandida === id ? null : id);
     };
 
+    //altera o estado dos status, "pendente", "agendado" ou "concluido"
+    const handleFiltroStatus = (novoStatus) => {
+        setFiltroStatus(novoStatus);
+        setMenuStatusAberto(false);
+    };
+
+    //desenhando na tela
     return (
         <div className={styles.tableWrapper}>
             <div className={styles.container}>
@@ -54,16 +125,51 @@ function Dashboard() {
                             <th>Como Conheceu:</th>
                             <th>Dt. de Criação:</th>
                             <th>Qtde. Filhos</th>
-                            <th className={styles.filterHeader}>Status</th>
-                            <th className={styles.filterHeader}>Agendamento</th>
+                            {/* meus filtros status e agendamento */}
+                            {/* onclick: responsável por "apagar" ou "ascender" a combobox */}
+                            <th className={styles.filterHeader} onClick={() => setMenuStatusAberto(!menuStatusAberto)}>
+                                Status <span className={styles.taskArrow}>v</span>
+                                {/* se menustatus é verdadeiro ele executa os () */}
+                                {menuStatusAberto && (
+                                    // não deixa o onclick se propagar pro > statyus v
+                                    <div className={styles.filter} onClick={(e) => e.stopPropagation()}>
+                                        {/* mapeando todos os estados */}
+                                        {["todos", "pendente", "agendado", "concluido"].map((st) => (
+                                            // passando cada estado pra key e aplicando a class e o onclick
+                                            <div key={st} className={styles.filterOption} onClick={() => handleFiltroStatus(st)}>
+                                                {/* e aqui os inputs que sao os "radios" que checam se o que eu cliquei é um st da lista mapeanda */}
+                                                <input type="radio" name="status" checked={filtroStatus === st} readOnly />
+                                                <label>{st.charAt(0).toUpperCase() + st.slice(1)}</label>
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                            </th>
+                            {/* mesma lógica dos status */}
+                            <th className={styles.filterHeader} onClick={() => setMenuDataAberto(!menuDataAberto)}>
+                                Agendamento <span className={styles.taskArrow}>v</span>
+                                {menuDataAberto && (
+                                    <div className={styles.filter} onClick={(e) => e.stopPropagation()}>
+                                        <span style={{color: 'white', fontSize: '12px'}}>Data específica:</span>
+                                        {/* abrindo o input de data */}
+                                        <input type="date" className={styles.inputDate} />
+                                        {/* botão de aplicar */}
+                                        <Button className={styles.btnFiltrar} onClick={() => setMenuDataAberto(false)}>Aplicar</Button>
+                                    </div>
+                                )}
+                            </th>
                             <th></th>
                         </tr>
                     </thead>
                     
                     <tbody>
-                        {agendamentos.map((item) => (
+                        {/* mapeio os status */}
+                        {agendamentosFiltrados.map((item) => (
+                            // passo o id dos tatus pra key
                             <React.Fragment key={item.id}>
-                                <tr className={styles[`row${item.status.charAt(0).toUpperCase() + item.status.slice(1)}`]}>
+                                {/* aqui estou criando classes para os meus 3 tipos de status, estou juntando a palavra row+(pendente, agendado ou concluido) ao invés de criar 3 classes diferentes */}
+                                <tr className={styles[`row${(item.status || "Pendente").charAt(0).toUpperCase() + (item.status || "Pendente").slice(1)}`]}>
+                                    {/* trazendo as linhas do banco */}
                                     <td>{item.id}</td>
                                     <td>{item.responsavel}</td>
                                     <td>{item.cpf}</td>
@@ -72,7 +178,47 @@ function Dashboard() {
                                     <td>{item.dataCriacao}</td>
                                     <td>{item.quantidadeFilhos}</td>
                                     <td>{item.status}</td>
-                                    <td>{item.dataAgendamento || "📅"}</td>
+                                    {/* lógica do agendamento */}
+                                    <td className={styles.colAgendamento}>
+                                        {/* o id dessa linha é o mesmo q cliquei pra editar? */}
+                                        {editandoId === item.id ? (
+                                            // se sim vem pra cá: nao deixo que o clique do input se propague pro pai
+                                            <div className={styles.editAgendamento} onClick={(e) => e.stopPropagation()}>
+                                                {/* abre a janela pr escolher a data e salvo com onchange */}
+                                                <input 
+                                                    type="date" 
+                                                    className={styles.inputDatePequeno}
+                                                    onChange={(e) => setDataInput(e.target.value)}
+                                                />
+                                                {/* faço o mesmo com a hora */}
+                                                <input 
+                                                    type="text" 
+                                                    placeholder="00:00h"
+                                                    className={styles.inputHora}
+                                                    onChange={(e) => setHoraInput(e.target.value)}
+                                                />
+                                                {/* botoes pra salvar ou cancelar edição */}
+                                                <div className={styles.editButtons}>
+                                                    <button onClick={() => salvarEdicao(item.id, "agendado")}>✓</button>
+                                                    <button onClick={() => setEditandoId(null)}>✗</button>
+                                                    <button onClick={() => salvarEdicao(item.id, "concluido")}>Done</button>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <div className={styles.agendamentoVisual} onClick={() => setEditandoId(item.id)}>
+                                                {item.dataAgendamento ? (
+                                                    <div className={styles.agendamentoContainer}>
+                                                        {item.dataAgendamento.split(' ').map((texto, index) => (
+                                                            <span key={index}>{texto}</span>
+                                                        ))}
+                                                    </div>
+                                                ) : (
+                                                    <img src={CalendarIcon} alt="calendário" className={styles.calendarIconCenter} />
+                                                )}
+                                            </div>
+                                        )}
+                                    </td>
+                                    {/* função que eu criei em /components do botão */}
                                     <td>
                                         <Button onClick={() => toggleLinha(item.id)} className={styles.btnAlunos}>
                                             Alunos {">"}
@@ -80,11 +226,12 @@ function Dashboard() {
                                     </td>
                                 </tr>
 
-                                {/* linha filhos */}
+                                {/* verifico se a linha que quero expandir é o id que eu cliquei */}
                                 {linhaExpandida === item.id && (
                                     <tr className={styles.rowExpanded}>
                                         <td colSpan="10">
                                             <div className={styles.filhosLista}>
+                                                {/* mapeando a lista de filhos para cada id encontrado */}
                                                 {item.filhos.map((filho, index) => (
                                                     <p key={index}>• {filho}</p>
                                                 ))}
@@ -100,4 +247,5 @@ function Dashboard() {
         </div>
     );
 }
+
 export default Dashboard;
